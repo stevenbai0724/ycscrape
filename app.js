ad714ccf52477e72fe2126235d88b337da21bd47
Steven Bai <stevenbai@richmedia.com>
2023-06-08 16:20:26 -0400

    puppeteer user simulation

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
-function openImage(url) {
-    const newWindow = window.open();
-    newWindow.document.write(`<html><body><img src="${url}" /></body></html>`);
-    newWindow.document.close();
-  }
 
+function delay(ms) {
+return new Promise((resolve) => setTimeout(resolve, ms));
+}
 async function scrapeWebsite(url) {
     try {
-        const res = await axios.get(url);
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+      
+        await page.goto(url); // Replace with the URL of the website
+      
+        // Scroll to the bottom of the page
+        await page.evaluate(() => {
+          window.scrollTo(0, document.body.scrollHeight);
+        });
+      
+        // Wait for some time to allow the content to load
+        await delay(2000); // Adjust the timeout as needed
+
+        await page.evaluate(() => {
+            window.scrollTo(0, document.body.scrollHeight);
+          });
+        
+          // Wait for some time to allow the content to load
+          await delay(2000); // Adjust the timeout as needed
+          
+        const htmlContent = await page.content();
+
+        await browser.close();
 
 
-        const $ = cheerio.load(res.data);
-
-
+        const $ = cheerio.load(htmlContent);
 
         const pageTitle = $('title').text();
         console.log('Page Title:', pageTitle);
 
-        const tag = 'img';
-        const className = 'user-gravatar';
+        const tag = 'a';
+        const className = 'WxyYeI15LZ5U_DOM0z8F';
 
         $(`${tag}.${className}`).each((index, element) => {
-            const src = $(element).attr('src');
-            openImage(src);
+            const src = $(element).attr('href');
+            console.log(src)
         })
 
 
@@ -35,4 +54,4 @@ async function scrapeWebsite(url) {
 }       
         
 
-scrapeWebsite("https://dmoj.ca/user/stevenbai0724")
+scrapeWebsite("https://www.ycombinator.com/companies")
