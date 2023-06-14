2174c61990ef56de2013a063e35b69e920222c66
Steven Bai <stevenbai@richmedia.com>
2023-06-14 14:59:30 -0400

    get the company description from list

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ async function scrapeWebsite(url) {
       
         await page.goto(url); // Replace with the URL of the website
         
-
-        for (var i = 0; i < 20; i++) {
+        //scroll to the bottom of the page, wait for 2 seconds to load
+        for (var i = 0; i < 2; i++) {
             await page.evaluate(() => {
                 window.scrollTo(0, document.body.scrollHeight);
               });
-            
-              // Wait for some time to allow the content to load
-              await delay(2000); // Adjust the timeout as needed
+
+            // Wait for some time to allow the content to load
+            await delay(2000); // Adjust the timeout as needed
         }
         // Scroll to the bottom of the page
 
@@ -37,21 +37,47 @@ async function scrapeWebsite(url) {
 
         const tag = 'a';
         const className = 'WxyYeI15LZ5U_DOM0z8F';
+        var cnt = 0;
+
+        const pclass = "whitespace-pre-line";
+        const ptag = "p";
+
+        var links = [];
 
         $(`${tag}.${className}`).each((index, element) => {
+
             const src = $(element).attr('href');
             if(!map.get(src)) {
-                
-                map.set(src, true);
-                console.log("https://www.ycombinator.com/companies" + src)
-            }
+    
 
-        })
+                map.set(src, true);
+                console.log(cnt + ". https://www.ycombinator.com" + src);
+                cnt++;
+                links.push(`https://www.ycombinator.com${src}`)
+                
+        }})             
+        var len = links.length;
+        for(var i = 0; i < len; i++) {
+
+            const page2 = await axios.get(links[i]);
+            const $ = cheerio.load(page2.data);
+
+            const title = $('title').text();
+            console.log(title);
+
+            $(`${ptag}.${pclass}`).each((index, element) => {
+
+                const text = $(element).text();
+
+                console.log(text);
+            
+            })   
+        
+        }
 
 
     } catch (err) {
         console.log("error: " + err.message);
-
     }   
 }       
         
